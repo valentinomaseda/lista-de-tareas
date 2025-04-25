354936493f8e8044c93169809d4476d47b1c232f
valentinomaseda <valentinomaseda@outlook.com>
2025-04-25 20:24:35 -0300

    agregué el js con cambios en el html y css

---
+++ script.js
@@ -0,0 +1,24 @@
+function addTask() {
+    const taskInput = document.getElementById("task");
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const listItem  = document.createElement("li");
+
+        const span = document.createElement("span");
+        span.textContent = taskText;
+        /* desarrollar que se marque como terminada */ 
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "X";
+        deleteButton.className = "deleteButton";
+        /* desarrollar que se elimine */ 
+
+        listItem.appendChild(span);
+        listItem.appendChild(deleteButton);
+
+        document.getElementById("taskList").appendChild(listItem);
+
+        taskInput.value = "";
+    }
+}
